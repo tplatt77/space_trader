5904004b816b8d86656fb191ee1c1ec2deab1080
TomPlatt <ax7gator@yahoo.com>
2017-02-26 15:53:42 -0500

    Missile improvements

--- core/src/com/mygdx/spacetrader/SpaceTrader.java
+++ core/src/com/mygdx/spacetrader/SpaceTrader.java
@@ -10,6 +10,9 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.mygdx.spacetrader.vehicle.Vehicle;
+import com.mygdx.spacetrader.missile.Missile;
+
+import java.util.ArrayList;
 
 public class SpaceTrader extends Game {
 	SpriteBatch batch;
@@ -23,18 +26,29 @@ public class SpaceTrader extends Game {
 	Vector2 velocity;
 	Vehicle playerVehicle;
     Vehicle enemyVehicle;
+	float playerRotation;
 
     //Missile data to be extracted for its own class in the future
     Texture missileImage;
     Sprite missileSprite;
     Vector2 missilePosition;
     boolean missileFired;
-	
+    float missileRotation;
+    ArrayList<Missile> missiles;
+
+    float animationTime;
+
 	@Override
 	public void create () {
+        animationTime = 0.0f;
+        missiles = new ArrayList<Missile>();
+
 		batch = new SpriteBatch();
 		img = new Texture("spaceship.png");
         enemyImage = new Texture("enemy.png");
+
+		playerRotation = 0.0f;
+        missileRotation = 0;
 
 		//viewport = new ExtendViewport(100, 100);
 		sprite = new Sprite(img);
@@ -82,28 +96,56 @@ public class SpaceTrader extends Game {
 		if(Gdx.input.isKeyPressed(Input.Keys.SPACE))
 		{
 			sprite.rotate(5.0f);
+			playerRotation += 5.0f;
 		}
 
         if(Gdx.input.isKeyPressed(Input.Keys.ENTER))
         {
+            float previousAnimationTime = animationTime;
+            animationTime += Gdx.graphics.getDeltaTime();
+
+
             missilePosition.x = position.x;
             missilePosition.y = position.y;
             missileFired = !missileFired;
             missileSprite.setPosition(missilePosition.x, missilePosition.y);
+            missileRotation = playerRotation;
+            missileSprite.setRotation(missileRotation);
+            if(animationTime >= 0.9f) {
+                missiles.add(new Missile("Missile", 100.0, 10.0, missileImage, missileSprite, missilePosition, missileRotation));
+            }
         }
-		batch.begin();
+
+        playerVehicle.setSpriteToPosition(position.x, position.y);
+
+        batch.begin();
 		//batch.draw(img, position.x, position.y);
 		//sprite.setPosition(position.x, position.y);
 		//sprite.setScale(0.5f);
 		//sprite.draw(batch);
 
-        playerVehicle.setSpriteToPosition(position.x, position.y);
         //enemyVehicle.setSpriteToPosition(10.0f, 10.0f);
-        playerVehicle.draw(batch);
-        enemyVehicle.draw(batch);
-        if(missileFired) {
-            missileSprite.draw(batch);
+        //enemyVehicle.draw(batch);
+        if(!missiles.isEmpty()) {
+            for(Missile missile: missiles) {
+                Vector2 thisMissilePosition = missile.getPosition();
+                double missileSpriteWidth = missile.getSprite().getWidth();
+                double missileSpriteHeight = missile.getSprite().getHeight();
+                // "Collision detection"
+                if(thisMissilePosition.x >= Gdx.graphics.getWidth() + missileSpriteWidth/2 ||
+                        thisMissilePosition.x + missileSpriteWidth/2 <= 0) {
+                    missiles.remove(missile);
+                    break;
+                }
+                else if(thisMissilePosition.y >= Gdx.graphics.getHeight() + missileSpriteHeight/2 ||
+                        thisMissilePosition.y  + missileSpriteHeight/2 <= 0) {
+                    missiles.remove(missile);
+                    break;
+                }
+                missile.draw(batch);
+            }
         }
+        playerVehicle.draw(batch);
 		batch.end();
 	}
 
---
+++ core/src/com/mygdx/spacetrader/missile/Missile.java
@@ -0,0 +1,101 @@
+package com.mygdx.spacetrader.missile;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Gator King on 2/25/2017.
+ */
+
+public class Missile {
+    private String name;
+    private double hitpoints;
+    private double speed;
+    private Texture image;
+    private Sprite sprite;
+    private Vector2 position;
+    private double rotationAngle;
+
+    public Missile() {
+
+    }
+
+    public Missile(Missile missile) {
+        this.name = missile.getName();
+        this.hitpoints = missile.getHitpoints();
+        this.speed = missile.getSpeed();
+        this.image = missile.getImage();
+        this.sprite = missile.getSprite();
+        this.position = missile.getPosition();
+    }
+
+    public Missile(String name, double hitpoints, double speed, Texture image, Sprite sprite, Vector2 position, double rotationAngle) {
+        this.name = name;
+        this.hitpoints = hitpoints;
+        this.speed = speed;
+        this.image = image;
+        this.sprite = sprite;
+        this.position = position;
+        this.rotationAngle = rotationAngle;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getHitpoints() {
+        return hitpoints;
+    }
+
+    public void setHitpoints(double hitpoints) {
+        this.hitpoints = hitpoints;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public Texture getImage() {
+        return image;
+    }
+
+    public void setImage(Texture image) {
+        this.image = image;
+    }
+
+    public Sprite getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Sprite sprite) {
+        this.sprite = sprite;
+    }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
+
+    public void draw(SpriteBatch batch) {
+        sprite.draw(batch);
+        double rotationAngleRadians = (rotationAngle*Math.PI)/180.0 + Math.PI/2;
+        position.x += 2.0f * Math.cos(rotationAngleRadians);
+        position.y += 2.0f * Math.sin(rotationAngleRadians);
+        sprite.setPosition(position.x, position.y);
+
+    }
+
+}
